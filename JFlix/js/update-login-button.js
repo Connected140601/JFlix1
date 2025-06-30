24abd6ccdb237f344cd3fc83003a6a8daf8d948c
Connected140601 <junrel.sapantaicloud@gmail.com>
2025-06-30 19:58:47 +0800

    Delete JFlix directory

--- JFlix/js/update-login-button.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const loginButton = document.getElementById('login-button-nav');
-    const user = JSON.parse(localStorage.getItem('user'));
-
-    if (user && user.email) {
-        loginButton.innerHTML = `<i class="fas fa-user"></i> ${user.email}`;
-    }
-});
